eaf2458ab7f40bb4b75037cf76cade1b4afb3c42
Mike <ampgzo2@gmail.com>
2022-11-10 11:40:36 -0500

    typeormmodule added to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import TypeOrmModule
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // create TypeOrmModule.forRoot objects in imports array
+    // type, host, port, username, password, database, enetites, and 
+    // synchronize
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'xxxx',
+      database: 'fruit-tree',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
